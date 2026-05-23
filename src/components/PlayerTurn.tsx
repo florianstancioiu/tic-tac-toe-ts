12a4a6bdd14f72f990ac86562a1d508a320127d5
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2026-05-23 10:58:33 +0300

    Made box shadows work again

--- src/components/PlayerTurn.tsx
+++ src/components/PlayerTurn.tsx
@@ -9,7 +9,7 @@ const PlayerTurn = ({ turn }: PlayerTurnProps) => {
   const imageType = turn === "x" ? smallXIcon : smallOIcon;
 
   return (
-    <div className="flex items-center justify-center gap-2.5 bg-semi-dark-navy shadow-box-dark-navy rounded-md h-10 w-24 -ml-8">
+    <div className="flex items-center justify-center gap-2.5 bg-semi-dark-navy shadow-box-dark-navy rounded-md h-10 w-24 -ml-8 pb-2">
       <img src={imageType} alt="" />
       <p className="text-silver uppercase">Turn</p>
     </div>
